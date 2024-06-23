5065de89b0c18d272b6d5e21883f6f80dc00654e
Brian Putnam <bkputnam@gmail.com>
2024-06-22 22:17:51 -0600

    Update devserver script to automatically install node_modules

--- devserver.sh
+++ devserver.sh
@@ -2,8 +2,8 @@
 
 ./build_all.sh
 
-# This isn't covered by build_all.sh because it isn't deployed to prod
-tsc -p local/devserver/tsconfig.json
-
+cd local/devserver;
+npm install --silent;
+tsc;
 # Add --inspect-brk to next command to debug devserver locally
-node local/devserver/src/devserver.js
+node src/devserver.js;
