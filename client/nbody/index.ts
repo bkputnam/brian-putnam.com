e5c808787b63a087804ab5eeda473c2b84a62cc4
Brian Putnam <bkputnam@gmail.com>
2024-10-11 10:53:17 -0600

    Test RK4 with static data

--- client/nbody/index.ts
+++ client/nbody/index.ts
@@ -3,26 +3,48 @@ import { getRed, setRgb } from "./util/imagedata_util.js";
 import { InitOutput, Universe } from "./wasm/nbody_wasm.js";
 
 class NBodySimulation {
-    private readonly wasm: InitOutput;
-    private readonly universe: Universe;
     private readonly canvas: HTMLCanvasElement;
     private readonly ctx: CanvasRenderingContext2D;
 
-    private constructor(initObj: InitResult) {
-        const { wasm, universe } = initObj;
-        this.wasm = wasm;
-        this.universe = universe;
+    private startMs: number;
+    private curIndex = 0;
+
+    private constructor(private readonly data: Array<Array<number>>) {
         this.canvas = document.createElement('canvas');
         this.canvas.width = document.body.clientWidth;
         this.canvas.height = document.body.clientHeight;
         document.body.appendChild(this.canvas);
         this.ctx = this.canvas.getContext("2d")!;
+
+        this.startMs = performance.now();
     }
 
     static async init(): Promise<NBodySimulation> {
-        const result = new NBodySimulation(await initUniverse());
-        requestAnimationFrame(() => result.drawLoop());
-        return result;
+        try {
+            const response = await fetch("./data.csv");
+            if (!response.ok) {
+                throw new Error(`Response status: ${response.status}`);
+            }
+
+            const responseText = await response.text();
+            const lines = responseText.split('\n').filter((line) => line != "");
+            let expectedLineLen = -1;
+            const data = lines.map((line: string) => {
+                const row =
+                    line.split(',').map((cell: string) => parseFloat(cell));
+                if (expectedLineLen === -1) {
+                    expectedLineLen = row.length;
+                } else if (row.length !== expectedLineLen) {
+                    throw new Error(`Unexpected row length: ${row.length}`);
+                }
+                return row;
+            });
+            const result = new NBodySimulation(data);
+            requestAnimationFrame(() => result.drawLoop());
+            return result;
+        } catch (error: unknown) {
+            return Promise.reject(error);
+        }
     }
 
     private xToCanvasCoords(x: number) {
@@ -38,39 +60,50 @@ class NBodySimulation {
             && (y >= 0 && y < this.canvas.height);
     }
 
+    private getPositions(): number[] {
+        const elapsedMs = (performance.now() - this.startMs) / 1000;
+        const distToActualElapsed =
+            (i: number) => Math.abs(this.data[i][0] - elapsedMs);
+        const curDistToElapsed = distToActualElapsed(this.curIndex);
+
+        while (true) {
+            if (this.curIndex == this.data.length - 1) {
+                this.curIndex = 0;
+                this.startMs = performance.now();
+                break;
+            }
+            if (distToActualElapsed(this.curIndex + 1) > curDistToElapsed) {
+                break;
+            }
+            this.curIndex++;
+        }
+        const curRow = this.data[this.curIndex];
+        return curRow.filter((val, index) => {
+            let indexWithinBody = (index - 1) % 5;
+            return indexWithinBody === 0 || indexWithinBody === 1;
+        });
+    }
+
     private draw() {
-        const num_bodies = this.universe.get_num_bodies();
-        const { wasm, universe, ctx, canvas } = this;
+        const { ctx, canvas } = this;
 
-        // Not sure why we have to recreate these every time. In previous versions
-        // of the code initializing these once, outside this function, worked just
-        // fine. However with commit 1be55af I started getting error messages about
-        // "attempting to access detached ArrayBuffer". I tracked it down to the
-        // line in QuadTree::new where we allocate the `nodes` array using
-        // `Vec::with_capacity`. I don't understand what that does to the
-        // `positions` and `masses` arrays, but if I comment it out I don't get the
-        // "detached ArrayBuffer" messages, and if I uncomment it I have to
-        // re-initialize these Float64Arrays on every draw() call as a workaround.
-        const positions = new Float64Array(
-            wasm.memory.buffer,
-            universe.get_positions(),
-            num_bodies * 2);
-        const masses = new Float64Array(
-            wasm.memory.buffer, universe.get_masses(), num_bodies);
+        const positions = this.getPositions();
+        const num_bodies = (positions.length - 1) / 5;
 
-        ctx.clearRect(0, 0, canvas.width, canvas.height);
+        this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
 
-        ctx.fillStyle = "#000";
-        ctx.fillRect(0, 0, canvas.width, canvas.height);
+        this.ctx.fillStyle = "#000";
+        this.ctx.fillRect(0, 0, this.canvas.width, this.canvas.height);
 
         let imageData = ctx.getImageData(0, 0, canvas.width, canvas.height);
         for (let i = 0; i < num_bodies; i++) {
-            const x = this.xToCanvasCoords(positions[i * 2]);
-            const y = this.yToCanvasCoords(positions[i * 2 + 1]);
+            const x = Math.round(this.xToCanvasCoords(positions[i * 2]));
+            const y = Math.round(this.yToCanvasCoords(positions[i * 2 + 1]));
             if (this.isInBounds(x, y)) {
-                let red = getRed(imageData, x, y);
-                red = Math.round(255 - (255 - red) * 0.5);
-                setRgb(imageData, x, y, red, red, red);
+                // let red = getRed(imageData, x, y);
+                // red = Math.round(255 - (255 - red) * 0.5);
+                // setRgb(imageData, x, y, red, red, red);
+                setRgb(imageData, x, y, 255, 255, 255);
             }
         }
         ctx.putImageData(imageData, 0, 0);
@@ -78,7 +111,7 @@ class NBodySimulation {
 
     private drawLoop() {
         this.draw();
-        this.universe.step_n(1);
+        // this.universe.step_n(1);
         requestAnimationFrame(() => this.drawLoop());
     }
 }
