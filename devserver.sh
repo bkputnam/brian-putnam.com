15c418e9f2b61640ee33bdf950d7a2c8b29cab46
Brian Putnam <bkputnam@gmail.com>
2024-10-17 09:31:55 -0600

    Update devserver.sh to only run npm install as needed
    This should also cut down on the annoying "ExperimentalWarning: Support for loading ES Module in require()..." warning message

--- devserver.sh
+++ devserver.sh
@@ -3,7 +3,16 @@
 ./build_all.sh
 
 cd local/devserver;
-npm install --silent;
+
+# Use node_modules/.package-lock.json as a signal for whether or not npm install
+# needs to be run. If it doesn't exist or if it's out of date (older than
+# package.json), then run npm install.
+if ! test -f node_modules/.package-lock.json || test package.json -nt node_modules/.package-lock.json; then
+    echo "node_modules out of date: running npm install";
+    npm install;
+fi
+
 tsc;
 # Add --inspect-brk to next command to debug devserver locally
 node src/devserver.js;
+
