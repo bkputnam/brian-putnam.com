039ccd9d5e77ecb79b8c96ee1d45e2b2618e0c57
Brian Putnam <bkputnam@gmail.com>
2024-12-01 14:13:00 -0700

    Saving work - crossword generator

--- client/crossword_generator/index.ts
+++ client/crossword_generator/index.ts
@@ -1,1 +1,4 @@
-alert('Hello world');
+import { PageController } from "./controllers/page_controller.js";
+
+const pageController = new PageController();
+document.body.appendChild(await pageController.render());
