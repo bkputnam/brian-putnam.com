297252b7ff0f4f2045dc3951ae532193f561da25
Brian Putnam <bkputnam@gmail.com>
2024-12-17 15:24:37 -0700

    Working version of count_inversions merge_sort

--- local/count_inversions/main.ts
+++ local/count_inversions/main.ts
@@ -2,8 +2,35 @@ import { bruteForce } from "./brute_force.js";
 import { mergeSort } from "./merge_sort.js";
 
 
-const arr = [4, 3, 2, 1];
+function randTest(): number[] {
+    const arrLen = Math.round(Math.random() * 10000);
+    const arr = new Array(arrLen);
+    for (let i = 0; i < arrLen; i++) {
+        arr[i] = Math.round(Math.random() * 100);
+    }
+    return arr;
+}
 
-console.log(bruteForce(arr));
+const tests = [
+    [ 89, 1, 27, 67 ],
+    [ 89, 65, 83, 30 ]
+];
 
-console.log(mergeSort(arr));
+let index = 0;
+while (true) {
+    const arr = tests.length > 0 ? tests.shift()! : randTest();
+
+    const expected = bruteForce([...arr]);
+    const actual = mergeSort([...arr]);
+
+    if (expected !== actual) {
+        console.log(arr);
+        console.log(expected);
+        console.log(actual);
+        break;
+    } else {
+        console.log(`${index}: SUCCESS ${expected} === ${actual}`);
+    }
+    index++;
+}
+
--- local/count_inversions/merge_sort.ts
+++ local/count_inversions/merge_sort.ts
@@ -1,23 +1,21 @@
-export function mergeSort(arr: number[]) {
-    const arrCopy = [...arr];
-    mergeSortHelper(arrCopy);
-    console.log(arrCopy);
-    return arrCopy;
+export function mergeSort(arr: number[]): number {
+    return mergeSortHelper([...arr]);
 }
 
-function mergeSortHelper(arr: number[]) {
+function mergeSortHelper(arr: number[]): number {
     if (arr.length <= 1) {
-        return;
+        return 0;
     }
     const midpoint = Math.round(arr.length / 2);
     const firstHalf = arr.slice(0, midpoint);
     const secondHalf = arr.slice(midpoint, arr.length);
-    mergeSortHelper(firstHalf);
-    mergeSortHelper(secondHalf);
+    const leftInversions = mergeSortHelper(firstHalf);
+    const rightInversions = mergeSortHelper(secondHalf);
     let firstHalfIndex = 0;
-    const firstHalfEnd = midpoint;
-    let secondHalfIndex = midpoint;
-    const secondHalfEnd = arr.length;
+    const firstHalfEnd = firstHalf.length;
+    let secondHalfIndex = 0;
+    const secondHalfEnd = secondHalf.length;
+    let crossInversions = 0;
     for (let i = 0; i < arr.length; i++) {
         const useFirstHalf = (() => {
             if (firstHalfIndex >= firstHalfEnd) {
@@ -26,7 +24,12 @@ function mergeSortHelper(arr: number[]) {
             if (secondHalfIndex >= secondHalfEnd) {
                 return true;
             }
-            return firstHalf[firstHalfIndex] <= secondHalf[secondHalfIndex];
+            const useFirstHalf =
+                firstHalf[firstHalfIndex] <= secondHalf[secondHalfIndex];
+            if (!useFirstHalf) {
+                crossInversions += firstHalfEnd - firstHalfIndex;
+            }
+            return useFirstHalf;
         })();
         if (useFirstHalf) {
             arr[i] = firstHalf[firstHalfIndex];
@@ -36,91 +39,6 @@ function mergeSortHelper(arr: number[]) {
             secondHalfIndex++;
         }
     }
+
+    return leftInversions + rightInversions + crossInversions;
 }
-
-
-// export function mergeSort(arr: number[]): number {
-//     const inputSlice = new Slice([...arr], 0, arr.length);
-//     const outputArr = new Array(arr.length);
-//     const outputSlice = new Slice(outputArr, 0, arr.length);
-//     const result = mergeSortHelper(inputSlice, outputSlice);
-//     console.log(outputArr);
-//     return result;
-// }
-
-// class Slice {
-//     constructor(
-//         private readonly arr: number[],
-//         readonly startIndex: number,
-//         readonly endIndex: number
-//     ) {}
-
-//     get(index: number): number {
-//         const trueIndex = this.startIndex + index;
-//         if (trueIndex >= this.endIndex) {
-//             throw new Error(
-//                 `Index out of bounds: ${trueIndex} >= ${this.endIndex}`);
-//         }
-//         return this.arr[trueIndex];
-//     }
-
-//     set(index: number, value: number) {
-//         const trueIndex = this.startIndex + index;
-//         if (trueIndex >= this.endIndex) {
-//             throw new Error(
-//                 `Index out of bounds: ${trueIndex} >= ${this.endIndex}`);
-//         }
-//         this.arr[trueIndex] = value;
-//     }
-
-//     length(): number {
-//         return this.endIndex - this.startIndex;
-//     }
-
-//     halve(): [Slice, Slice] {
-//         const midpoint = (this.startIndex + this.endIndex) / 2;
-//         return [
-//             new Slice(this.arr, this.startIndex, midpoint),
-//             new Slice(this.arr, midpoint, this.endIndex),
-//         ];
-//     }
-// }
-
-// function mergeSortHelper(inputSlice: Slice, outputSlice: Slice): number {
-//     const len = inputSlice.length();
-//     if (len <= 1) {
-//         return 0;
-//     }
-//     const [firstHalf, secondHalf] = inputSlice.halve();
-//     const [outputFirst, outputSecond] = outputSlice.halve();
-//     mergeSortHelper(firstHalf, outputFirst);
-//     mergeSortHelper(secondHalf, outputSecond);
-//     let outputIndex = 0;
-//     let firstHalfIndex = 0;
-//     const firstHalfEnd = firstHalf.length();
-//     let secondHalfIndex = 0;
-//     const secondHalfEnd = secondHalf.length();
-//     while (outputIndex < len) {
-//         const useFirstHalf = (() => {
-//             if (firstHalfIndex >= firstHalfEnd) {
-//                 return false;
-//             }
-//             if (secondHalfIndex >= secondHalfEnd) {
-//                 return true;
-//             }
-//             return (
-//                 firstHalf.get(firstHalfIndex) < secondHalf.get(secondHalfIndex)
-//             );
-//         })();
-//         if (useFirstHalf) {
-//             outputSlice.set(outputIndex, firstHalf.get(firstHalfIndex));
-//             firstHalfIndex++;
-//         } else {
-//             outputSlice.set(outputIndex, secondHalf.get(secondHalfIndex));
-//             secondHalfIndex++;
-//         }
-//         outputIndex++;
-//     }
-
-//     return 5;
-// }
